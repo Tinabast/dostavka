acf10aeb37b638bbd36fa976a0a2e0acf690b5ce
Tina <tinabast1987@gmail.com>
2014-12-01 22:27:44 +0400

    Adding hovers and basic logic

--- js/base.js
+++ js/base.js
@@ -1,6 +1,8 @@
 (function ($, window, undefined){
 
 $(function () {
+	var orderTop = $('.current-order').first().offset().top;
+
 	$('[data-image]').each(function () {
 		var $this = $(this);
 		
@@ -28,15 +30,43 @@ $(function () {
 	});
 	
 	/* ORDER */
-	$('.current-order .details-trigger').on('click', function () {
+	$('.current-order .details-trigger').on('click', function (e) {
 		var $this = $(this),
 			$order = $this.parents('.current-order');
+			
+		e.preventDefault();
 
 		if ($this.hasClass('show-details')) {
 			$order.addClass('with-details');
 		} else if ($this.hasClass('hide-details')) {
 			$order.removeClass('with-details');			
 		}
+	});
+	$(window).on('scroll', function () {
+		var currentTop = $(window).scrollTop(),
+			$order = $('.current-order').first();
+		
+		if (orderTop < currentTop) {
+			$order.addClass('fixed');
+		} else {
+			$order.removeClass('fixed');
+		}
+		
+		if (currentTop > 0) {
+			$('.button-to-top').removeClass('btn_disabled');
+		} else {
+			$('.button-to-top').addClass('btn_disabled');
+		}
+	});
+	
+	/* BUTTON TO TOP */
+	
+	$('.button-to-top').on('click', function () {
+		$topButton = $(this);
+
+		$("html, body").animate({scrollTop:0}, '500', 'swing', function() { 
+		   $topButton.addClass('btn_disabled');
+		});
 	});
 });
 
